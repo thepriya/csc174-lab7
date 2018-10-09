ac77fb1b18c08fd4d2154876109c363648508b51
Marcelo Gheiler <marcelogheiler@gmail.com>
2018-10-09 12:23:54 -0400

    php includes and reformat html and redo some css

---
+++ includes/navbar.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="student-detail.php#kevin">Kevin</a></li>
+            <li><a href="student-detail.php#lingling">Lingling</a></li>
+            <li><a href="student-detail.php#valerie">Valerie</a></li>
+        </ul>
+    </nav>
+    <h1><a href="index.php" class="normal-text">Odd Student Out</a></h1>
+</header>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>lab07 Odd Student Out</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/navigation.css">
+</head>
+
+<body>
+    <div class="container">
+        <?php include 'includes/navbar.php' ?>
+        <div class="wrapper">
+            <div class="section odd-out">
+                <div class="left-img">
+                    <img src="images/kevin.jpg" alt="kevin">
+                </div>
+                <h2 class="right-text">Kevin Kao</h2>
+                <p>
+                    Kevin Kao is a University of Rochester student who was born in New York but went to Taiwan for primary and secondary educations! He is currently a sophomore doubling major in Computer Science and Mathematics at the University of Rochester. He has a strong interest in the field of science, and he has taken many courses related to his major last year. For example, he took CSC (170, 171, 172) and MTH (150, 165, 171, 172). In addition to that, he is now taking CSC (173, 174, 214) and MTH (173, 190, 201) to better prepare him for his career. He would like to share what triggers him to become a copmuter scientist and why he loves technology so much!
+                </p>
+                <div><a href="student-detail.html#kevin">Read more about Kevin here</a></div>
+                <div><a href="http://csc174.org/assignment02/kkao4/">Original website of Kevin</a></div>
+            </div>
+            <div class="section">
+                <div class="right-img">
+                    <img src="images/lingling.jpg" alt="lingling">
+                </div>
+                <h2 class="left-text">Lingling Li</h2>
+                <p>
+                    Lingling Li is a University of Rochester student who is from Shanghai, China. Her English name is Lily. She loves playing the piano, drawing, cooking and playing the badminton. She is now a sophomore, and also the D’Lion in charge of Gates 6 and Gannett 6. Her job is to help the first year student better move in to the brand new college life, and be their first friend in the college. She is planning to have a double major in Business and Digital Media Study, minor in Japanese. A fun fact about her: She can never count how many steps she had done when doing the “Running jump shot”
+                </p>
+                <div><a href="student-detail.html#lingling">Read more about Lingling here</a></div>
+                <div><a href="http://csc174.org/assignment02/lli53/">Original website of Lingling</a></div>
+            </div>
+            <div class="section">
+                <div class="left-img">
+                    <img src="images/valerie.jpg" alt="valerie">
+                </div>
+                <h2 class="right-text">Valerie Tam</h2>
+                <p>
+                    Valerie Tam is a sophomore at the University of Rochester majoring in data science with clusters in Chinese and Psychology. She is also the marketing director of the Hong Kong Students’ Association. As the marketing director, she updates the club’s social media pages and creates posters for events. She was born and raised in New York City. Some of her favorite things to do in New York City include walking around Soho and going to the best bubble tea shops, such as Bar Pa Tea and Yaya Tea. In addition, she considers herself a foodie, always trying the newest restaurant or dessert shop which is why she created a foodstagram to document all of her food journeys.
+                </p>
+                <div><a href="student-detail.html#valerie">Read more about Valerie here</a></div>
+                <div><a href="http://csc174.org/assignment02/vtam/">Original website of Valerie</a></div>
+            </div>
+        </div>
+        <footer>
+        </footer>
+    </div>
+</body>
+
+</html>
